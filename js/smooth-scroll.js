602c994155e1225b9b07528cdbef66ac301bed2c
Kiil <Nikolajkiil@live.dk>
2018-05-28 12:42:04 +0200

    Added smooth scroll js
    The navigation now works with smoothscroll through JQuery

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,37 +1,16 @@
-var targetOffset, currentPosition,
-    body = document.body,
-    button = document.getElementById('scrollButton'),
-    animateTime = 900;
+//Eksempel er hentet fra denne hjemmeside: https://codepen.io/chrishammond/pen/rLJPzK
 
-function getPageScroll() {
-  var yScroll;
-
-  if (window.pageYOffset) {
-    yScroll = window.pageYOffset;
-  } else if (document.documentElement && document.documentElement.scrollTop) {
-    yScroll = document.documentElement.scrollTop;
-  } else if (document.body) {
-    yScroll = document.body.scrollTop;
+jQuery(document).ready(function($) {
+  // Scroll to the desired section on click
+  // Make sure to add the `data-scroll` attribute to your `<a>`          tag.
+  // Example: 
+  // `<a data-scroll href="#my-section">My Section</a>` will scroll to an element with the id of 'my-section'.
+  function scrollToSection(event) {
+    event.preventDefault();
+    var $section = $($(this).attr('href')); 
+    $('html, body').animate({
+      scrollTop: $section.offset().top
+    }, 500);
   }
-  return yScroll;
-}
-
-button.addEventListener('click', function (event) {
-
-  targetOffset = document.getElementById(event.target.hash.substr(1)).offsetTop;
-  currentPosition = getPageScroll();
-
-  body.classList.add('in-transition');
-  body.style.WebkitTransform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
-  body.style.MozTransform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
-  body.style.transform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
-
-  window.setTimeout(function () {
-    body.classList.remove('in-transition');
-    body.style.cssText = "";
-    window.scrollTo(0, targetOffset);
-  }, animateTime);
-
-  event.preventDefault();
-
-}, false);
+  $('[data-scroll]').on('click', scrollToSection);
+}(jQuery));
